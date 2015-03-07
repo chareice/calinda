52273a673b46296838ae57ca1639d8867b00d89c
Chareice <chareice@live.com>
2015-03-08 02:10:37 +0800

    update post

--- config.rb
+++ config.rb
@@ -5,6 +5,8 @@ set :images_dir, 'images'
 set :markdown_engine, :redcarpet
 set :markdown, :fenced_code_blocks => true, :tables => true, :autolink => true, :strikethrough => true, :superscript => true, :footnotes => true
 
+activate :asciidoc, :asciidoc_attributes => %w()
+
 configure :build do
   activate :minify_css
   activate :minify_javascript
